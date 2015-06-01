77c33f935b99bac7272a5634cf7763015df34a84
1101410004 <mis0405104456@gmail.com>
2015-06-01 16:27:51 +0800

    add views

--- ObserverSample/src/tw/edu/npu/mis/ObserverSample.java
+++ ObserverSample/src/tw/edu/npu/mis/ObserverSample.java
@@ -40,13 +40,11 @@ public class ObserverSample {
         Window window = new Window();
         Model model = new Model();
         Controller controller = new Controller(model);
-        List<View> views = new ArrayList<>();
+        List<Showable> views = new ArrayList<>();
         views.add(new View("View 1", window, model));
         views.add(new View("View 2", window, model));
         views.add(new View("View 3", window, model));
-   
-
-        // Start the event loop.
+        views.add(new AlternativeView("View",window,model));
         window.startEventLoop(controller, views);
     }
 }
